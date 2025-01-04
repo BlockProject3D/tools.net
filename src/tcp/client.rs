a95a98517a856d94ae2265254d092e08b63a5655
Yuri Edward <yuri6037@outlook.com>
2025-01-04 23:06:08 +0100

    Added initial version of TCP client implementation

---
+++ src/tcp/client.rs
@@ -0,0 +1,266 @@
+// Copyright (c) 2025, BlockProject 3D
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject 3D nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+//! A basic TCP client implementation designed for long-running connections.
+
+use tokio::sync::{mpsc, watch, Semaphore};
+use std::future::Future;
+use std::sync::Arc;
+use bp3d_debug::trace;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpStream, ToSocketAddrs};
+use tokio::select;
+use tokio::sync::mpsc::error::{SendError, TrySendError};
+use tokio::task::JoinHandle;
+use crate::tcp::util::Network;
+use crate::util::DataMsg;
+
+/// Represents the main event handler for a TCP client.
+pub trait Handler {
+    /// The type of event which can be received by this event handler.
+    type Event: Send + 'static;
+
+    /// Called when an event was received by the client.
+    ///
+    /// # Arguments
+    ///
+    /// * `event`: the received event
+    ///
+    /// returns: impl Future<Output=()>+Send+Sized
+    fn event(&mut self, _: Self::Event) -> impl Future<Output = ()> + Send {
+        async move {}
+    }
+
+    /// Called when data is pending to be received from the socket.
+    ///
+    /// # Arguments
+    ///
+    /// * `net`: the network context.
+    ///
+    /// returns: impl Future<Output=Result<(), Error>>+Send+Sized
+    fn recv(&mut self, net: &mut Network) -> impl Future<Output = std::io::Result<()>> + Send;
+
+    /// Called when the client task is about to return.
+    ///
+    /// # Arguments
+    ///
+    /// * `net`: the network context.
+    ///
+    /// returns: impl Future<Output=Result<(), Error>>+Send+Sized
+    fn disconnect(&mut self, _: &mut Network) -> impl Future<Output = std::io::Result<()>> + Send {
+        async move {
+            Ok(())
+        }
+    }
+}
+
+/// A factory trait which can be used to create the instance of the main event handler.
+pub trait Factory {
+    /// The type of the main event handler.
+    type Handler: Handler + Send + 'static;
+
+    /// Called when the client is about to start to create the corresponding event handler.
+    fn start(self, client: &Arc<Client<<Self::Handler as Handler>::Event>>) -> Self::Handler;
+}
+
+/// SAFETY: DataMsg must point to valid memory (normally ensured by Client structure).
+async unsafe fn handle_data(msg: DataMsg, net: &mut Network) -> std::io::Result<()> {
+    trace!({?net} {?msg}, "Received data event");
+    let slice = std::slice::from_raw_parts(msg.buffer, msg.buffer_size);
+    net.write_all(slice).await?;
+    net.flush().await?;
+    (*msg.synchro).add_permits(1);
+    Ok(())
+}
+
+/// The main builder structure used to create a new TCP client.
+pub struct Builder<F> {
+    factory: F,
+    event_queue_size: usize
+}
+
+impl<F: Factory> Builder<F> {
+    /// Creates a new TCP client from the given main event handler factory.
+    pub fn new(factory: F) -> Builder<F> {
+        Self {
+            factory,
+            event_queue_size: 4
+        }
+    }
+
+    /// Sets the size of the event queue.
+    ///
+    /// The default is 4.
+    ///
+    /// # Arguments
+    ///
+    /// * `size`: event queue size.
+    ///
+    /// returns: Builder<F>
+    pub fn event_queue_size(mut self, size: usize) -> Self {
+        self.event_queue_size = size;
+        self
+    }
+
+    /// Connects to the server with the specified address.
+    ///
+    /// # Arguments
+    ///
+    /// * `addr`: the address to connect to.
+    ///
+    /// returns: Result<ClientApp<<<F as Factory>::Handler as Handler>::Event>, Error>
+    ///
+    /// # Errors
+    ///
+    /// Returns an IO error if the client could not connect to the specified server.
+    pub async fn connect(self, addr: impl ToSocketAddrs) -> std::io::Result<ClientApp<<F::Handler as Handler>::Event>> {
+        let stream = TcpStream::connect(addr).await?;
+        let addr = stream.peer_addr()?;
+        let mut net = Network::new(0, stream, addr);
+        let (event_sender, mut event_receiver) = mpsc::channel(self.event_queue_size);
+        let (exit_sender, mut exit_receiver) = watch::channel(());
+        let (data_sender, mut data_receiver) = mpsc::channel(self.event_queue_size);
+        let client = Arc::new(Client {
+            exit: exit_sender,
+            event_sender,
+            data: data_sender
+        });
+        let mut handler = self.factory.start(&client);
+        let handle = tokio::spawn(async move {
+            loop {
+                select! {
+                    _ = exit_receiver.changed() => break,
+                    Some(event) = event_receiver.recv() => handler.event(event).await,
+                    res = net.ready() => {
+                        let ev = res?;
+                        if ev.is_error() || ev.is_read_closed() || ev.is_write_closed() {
+                            break;
+                        }
+                        if ev.is_readable() {
+                            handler.recv(&mut net).await?;
+                        }
+                    },
+                    Some(msg) = data_receiver.recv() => unsafe {
+                        handle_data(msg, &mut net).await?
+                    }
+                }
+            }
+            handler.disconnect(&mut net).await?;
+            Ok(())
+        });
+        Ok(ClientApp {
+            client,
+            handle
+        })
+    }
+}
+
+/// Represents a client with a long-running connection.
+pub struct Client<E> {
+    exit: watch::Sender<()>,
+    event_sender: mpsc::Sender<E>,
+    data: mpsc::Sender<DataMsg>
+}
+
+impl<E> Client<E> {
+    /// Requests exit of the server.
+    pub fn exit(&self) {
+        let _ = self.exit.send(());
+    }
+
+    /// Send an event to the main event handler from asynchronous code.
+    ///
+    /// # Arguments
+    ///
+    /// * `event`: the event to send.
+    ///
+    /// returns: Result<(), SendError<E>>
+    ///
+    /// # Errors
+    ///
+    /// Returns a SendError if the server has exited.
+    pub async fn event_async(&self, event: E) -> Result<(), SendError<E>> {
+        self.event_sender.send(event).await
+    }
+
+    /// Send an event to the main event handler from synchronous code.
+    ///
+    /// # Arguments
+    ///
+    /// * `event`:
+    ///
+    /// returns: Result<(), TrySendError<E>>
+    ///
+    /// # Errors
+    ///
+    /// Returns a TrySendError if the server has exited or if the event queue is full.
+    /// See [Builder] for more information on the configuration of the event queue.
+    pub fn event(&self, event: E) -> Result<(), TrySendError<E>> {
+        self.event_sender.try_send(event)
+    }
+
+    /// Send the given data buffer and flush the stream.
+    ///
+    /// # Arguments
+    ///
+    /// * `msg`: the message buffer to send.
+    ///
+    /// returns: true if the operation has succeeded, false otherwise.
+    pub async fn send(&self, msg: &[u8]) -> bool {
+        // SAFETY: It is safe to pass a pointer to msg as long as we wait for all clients to have
+        // consumed the pointer before returning.
+        let synchro = Semaphore::new(0);
+        if let Err(_) = self.data.send(DataMsg {
+            synchro: &synchro,
+            buffer: msg.as_ptr(),
+            buffer_size: msg.len(),
+            net_id: 0
+        }).await {
+            return false;
+        }
+        trace!("Waiting for the async task to acknowledge");
+        let _ = synchro.acquire().await.unwrap();
+        true
+    }
+}
+
+/// Represents a client application.
+pub struct ClientApp<E> {
+    handle: JoinHandle<std::io::Result<()>>,
+    client: Arc<Client<E>>
+}
+
+impl<E> ClientApp<E> {
+    /// Join and waits for the client to stop.
+    ///
+    /// Warning this does not automatically exit the client and will wait for a future call to the
+    /// [Client::exit] function before returning.
+    pub async fn join(self) -> std::io::Result<()> {
+        self.handle.await?
+    }
+}
